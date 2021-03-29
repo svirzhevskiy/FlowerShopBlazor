b6c9e23eff84206dd6cfb64c6e86d5cd80be5ff1
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-03-29 17:38:29 +0300

    Added Product and Category models

---
+++ FlowerShopBlazor/Models/CategoryModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FlowerShopBlazor.Models
+{
+    public class CategoryModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public List<string> Properties { get; set; }
+    }
+}
---
+++ FlowerShopBlazor/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FlowerShopBlazor.Models
+{
+    public class ProductModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public int Price { get; set; }
+        public int? OldPrice { get; set; }
+        public string Properties { get; set; }
+        
+        public CategoryModel Category { get; set; }
+    }
+}
