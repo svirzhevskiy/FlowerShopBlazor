c22bfc60e18ab50a4528c80f60f0f456af983c42
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-03-31 10:52:43 +0300

    Added AppState

---
+++ FlowerShopBlazor/Services/AppState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace FlowerShopBlazor.Services
+{
+    public class AppState
+    {
+        public event Action<ComponentBase, string> StateChanged;
+
+        private void NotifyStateChanged(ComponentBase source, string property) =>
+            StateChanged?.Invoke(source, property);
+    }
+}
