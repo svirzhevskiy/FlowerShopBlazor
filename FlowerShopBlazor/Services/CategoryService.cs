c2362d58c9abccb0793c810fbc9c5874a80ced89
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-04-14 16:13:45 +0300

    Fix CategoryService GetAll

--- FlowerShopBlazor/Services/CategoryService.cs
+++ FlowerShopBlazor/Services/CategoryService.cs
@@ -34,7 +34,6 @@ namespace FlowerShopBlazor.Services
             catch (Exception e)
             {
                 Console.WriteLine(e);
-                throw;
             }
             return new() {_fakeCategory};
         }
